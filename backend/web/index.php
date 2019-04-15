329d47e83573a32915c410f70bc131bf119d78c3
wangxiaoxiao <pla_wh95@163.com>
2019-04-15 16:54:40 +0800

    修改配置信息

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,6 +1,7 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', true);//标识应用是否开启调试模式
+defined('YII_ENV') or define('YII_ENV', 'dev');//标识应用运行的环境,默认生产(prod)
+//YII_ENABLE_ERROR_HANDLER 标识是否启用Yii提供的错误处理机制,默认true
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
